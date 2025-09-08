cee81d2fe6331d2ba6490afe23ce048d222a6139
Ashutosh-Alhodood <ashutosh@alhodood.com>
2025-09-08 17:31:30 +0530

    update

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",            // static export
-  images: { unoptimized: true }, // disable Next image optimizer for static hosting
-  trailingSlash: true          // makes pages end with / for static export (optional)
+  output: "export",
+  images: { unoptimized: true },
+  trailingSlash: true
 };
-
 module.exports = nextConfig;
