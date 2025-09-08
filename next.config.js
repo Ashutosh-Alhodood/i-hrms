258ffcd5a4a44b437601a76785685015407877b2
Ashutosh-Alhodood <ashutosh@alhodood.com>
2025-09-08 17:23:45 +0530

    update

--- next.config.js
+++ next.config.js
@@ -1,14 +1,8 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
-  reactStrictMode: true,
-  // allow dev origins (replace with the exact origin you use)
-  // include protocol and port
-  allowedDevOrigins: ['http://192.168.0.128:3000', 'http://localhost:3000'],
-  images: {
-    domains: [
-      'images.unsplash.com',
-      'randomuser.me',
-      // add any external domains you use
-    ],
-  },
-}
+const nextConfig = {
+  output: "export",            // static export
+  images: { unoptimized: true }, // disable Next image optimizer for static hosting
+  trailingSlash: true          // makes pages end with / for static export (optional)
+};
+
+module.exports = nextConfig;
